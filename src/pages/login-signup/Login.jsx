dc6694ca301c6d5e05a921a12bfbe9e6bf718d0c
bhaskar <bhaskar@nuitsolutions.com>
2024-03-21 20:00:28 +0530

    login page tab struct

--- src/pages/login-signup/Login.jsx
+++ src/pages/login-signup/Login.jsx
@@ -1,25 +1,45 @@
+import { useState } from "react";
 import LoginForm from "../../components/forms/Login-form";
 
 
 const Login = () => {
+
+
+    const [signup, setSignup] = useState(true);
+
+
+    const ShowLogin = ()=>{
+
+        // if(signup)
+    }
 
     return (
         <>
 
         <div className="flex">
             <div className="">
-                <button type="button">
+                <button type="button" onClick={ShowLogin}>
                     Sign Up
                 </button>
             </div>
             <div className="">
-                <button type="button">
+                <button type="button" onClick={ShowLogin}>
                     Log In
                 </button>
             </div>
         </div>
 
-            <LoginForm />
+        <div className="tab-content">
+            <div className="tabSignup">
+                <h1>
+                   Signup Goes Here 
+                </h1>
+            </div>
+            <div className="tabLogin">
+                <LoginForm />
+            </div>
+        </div>
+
 
         </>
     )
