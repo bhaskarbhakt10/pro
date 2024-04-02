eb462dd6ed017ff6aec3e3757ffc4b3cb817d610
bhaskar <bhaskar@nuitsolutions.com>
2024-04-02 19:06:56 +0530

    store steup

---
+++ src/slices/loginSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialstate = {};
+
+export const todoSlice = createSlice({
+    name: 'login',
+    initialstate,
+    reducers: {
+        loginUser: (state, action) => {
+
+        },
+        logoutUser: (state, action) => {
+
+        }
+    }
+});
+
